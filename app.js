b62e43e9f3dd141d57f1be0fd686391b76a2a1e1
adil abid <117192462+adil1942002@users.noreply.github.com>
2023-06-04 23:24:36 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+var inp = document.getElementById("inp")
+var ul = document.getElementById("ul")
+function delRow(btn){
+btn.parentNode.remove();
+}
+
+function Edit(btn){
+    var a = prompt("Edite")
+btn.parentNode.firstChild.nodeValue = a
+}
+function add(){
+    var li = document.createElement("li")
+    var inpval = document.createTextNode(inp.value)
+    li.appendChild(inpval)
+    ul.appendChild(li)
+    inp.value = ""
+
+
+var del = document.createElement("button")
+var deltext = document.createTextNode("Delete")
+del.appendChild(deltext)
+
+del.setAttribute("class","btn")
+del.setAttribute("onclick", "delRow(this)")
+del.style.marginLeft = "20px"
+
+li.appendChild(del);
+
+
+
+
+
+var editBtn = document.createElement("button")
+var editBtnVal = document.createTextNode("Edit")
+editBtn.appendChild(editBtnVal)
+editBtn.style.marginLeft = "10px"
+editBtn.style.padding = "10px"
+li.appendChild(editBtn)
+
+
+editBtn.setAttribute("onclick" , "Edit(this)")
+
+
+}
+
+
